bc7de80953ff37953cb731d662d448dfeca0f3a9
Dustin Stacy <dustin.stacy@outlook.com>
2024-06-26 12:54:57 -0500

    update enterLottery function

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -2,21 +2,31 @@
 
 pragma solidity ^0.8.18;
 
+error Lottery__NotEnoughEthSent();
+
 /**
  * @title A sample Lottery Contract
  * @author Dustin Stacy
  * @notice This contract is for creating a sample lottery
  * @dev Implents Chainlink VRFv2
  */
-
 contract Lottery {
     uint256 private immutable i_entranceFee;
+    address payable[] private s_entrants;
+
+    event LotteryEntered(address indexed entrant);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterLottery() public payable {}
+    function enterLottery() external payable {
+        if (msg.value < i_entranceFee) {
+            revert Lottery__NotEnoughEthSent();
+        }
+        s_entrants.push(payable(msg.sender));
+        emit LotteryEntered(msg.sender);
+    }
 
     function chooseWinner() public {}
 
