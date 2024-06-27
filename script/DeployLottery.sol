698bc1325eda2b07084e44836f0a042fcb66adc2
Dustin Stacy <dustin.stacy@outlook.com>
2024-06-27 10:53:51 -0500

    updated DeployLottery script

--- script/DeployLottery.sol
+++ script/DeployLottery.sol
@@ -4,9 +4,31 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {Lottery} from "../src/Lottery.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployLottery is Script {
     function run() external returns (Lottery lottery) {
-        // lottery = new Lottery();
+        HelperConfig helperConfig = new HelperConfig();
+        (
+            address vrfCoordinator,
+            bytes32 gasLane,
+            uint256 entranceFee,
+            uint256 interval,
+            uint64 subscriptionId,
+            uint32 callbackGasLimit
+        ) = helperConfig.activeNetworkConfig();
+
+        vm.startBroadcast();
+        lottery = new Lottery(
+            vrfCoordinator,
+            gasLane,
+            entranceFee,
+            interval,
+            subscriptionId,
+            callbackGasLimit
+        );
+        vm.stopBroadcast();
+
+        return lottery;
     }
 }
